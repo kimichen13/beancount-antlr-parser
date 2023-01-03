5094e9c1fee1ce7302d88abe1f5dbcf68745bee5
Kimi Chen <kimichen13@gmail.com>
2023-01-03 16:36:32 +0800

    Snapshot publish configuration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
+import java.net.URI
+
 plugins {
-    id("java")
-    id("groovy")
-    id("antlr")
+    java
+    groovy
+    antlr
+    `maven-publish`
+    signing
 }
 
 group = "com.qqviaja.tools"
@@ -29,4 +33,71 @@ dependencies {
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+artifacts {
+    archives(tasks.named("javadocJar"))
+    archives(tasks.named("sourcesJar"))
+}
+
+tasks.named("sourcesJar").configure { dependsOn("generateGrammarSource") }
+
+signing {
+    sign(configurations.archives.get())
+}
+
+publishing {
+
+    repositories {
+        maven {
+            val releaseRepo = URI("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotRepo = URI("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            url = if (!(version as String).endsWith("SNAPSHOT")) releaseRepo else snapshotRepo
+            credentials {
+                username = properties["nexusUsername"].toString()
+                password = properties["nexusPassword"].toString()
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("Sonatype") {
+            groupId = group.toString()
+            artifactId = rootProject.name
+            version = version
+
+            from(components["java"])
+
+            pom {
+                groupId = group.toString()
+                name.set("Beancount Antlr Parser")
+                description.set("A tool to parse input to beancount directives.")
+                url.set("https://github.com/kimichen13/beancount-antlr-parser")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://github.com/kimichen13/beancount-antlr-parser/blob/main/LICENSE.md")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("kimicubao")
+                        name.set("Kimi Chen")
+                        email.set("kimichen13@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git@github.com:kimichen13/beancount-antlr-parser.git")
+                    developerConnection.set("scm:git:ssh://github.com:kimichen13/beancount-antlr-parser.git")
+                    url.set("https://github.com/kimichen13/beancount-antlr-parser")
+                }
+            }
+        }
+    }
 }
